2ef78914d4e15dc2e5a8fdcbb6d863bf0d78affb
Carolina Borges <producaocarolsbo@gmail.com>
2018-09-20 20:27:05 -0300

    versão inical do meu projeto!!

---
+++ Main/Distancia entre pontos/__init__.py
@@ -0,0 +1,11 @@
+import math
+
+px1= int(input("Coordenada de px1: "))
+px2= int(input("Coordenada de px2: "))
+py1= int(input("Coordenada de py1: "))
+py2= int(intup("Coordenada de py2: "))
+d=math.sqrt ((px1-px2)**2+(py1-py2)**2)
+print("distnaica entre os pontos",d)
+
+
+
