41c3c39fa0d9c61bb20b64960c3e1c327d90dcd1
Carolina Borges <producaocarolsbo@gmail.com>
2018-09-20 21:17:09 -0300

    versão inical do meu projeto!!

---
+++ Area da circunferencia/__init__.py
@@ -0,0 +1,5 @@
+import math
+R=float (input("coloque o valor de R: "))
+c=(math.pi*R**2)
+print("valor de c é: ",c)
+
--- Main/Distancia entre pontos/__init__.py
+++ Main/Distancia entre pontos/__init__.py
@@ -5,7 +5,7 @@ px2= int(input("Coordenada de px2: "))
 py1= int(input("Coordenada de py1: "))
 py2= int(intup("Coordenada de py2: "))
 d=math.sqrt ((px1-px2)**2+(py1-py2)**2)
-print("distnaica entre os pontos",d)
+print("distancia entre os pontos",d)
 
 
 
